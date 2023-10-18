a1a67d47b40b5d10d684e2e5d7a7e6896e3df2c6
Kalavrat <97897883+Kalavrat@users.noreply.github.com>
2023-10-18 09:33:04 +0300

    Add files via upload

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,15 @@
 import "../../App.css";
 import Header from "../../components/header/Header";
+import style from "./MainPage.module.css"
+import { HandySvg } from "handy-svg";
+import logo from "../../media/logo.svg";
 function MainPage() {
   return (
     <div className="App">
       <Header />
+      <div className={style.main_head}>
+        <HandySvg className={style.main_head_logo} src={logo} alt="" />
+      </div>
     </div>
   );
 }
